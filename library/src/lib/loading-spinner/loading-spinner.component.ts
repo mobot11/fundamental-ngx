86729d7cfd9ade2923d701ce3dba4d553e0863d9
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-01-07 12:32:57 -0700

    automatically apply aria-busy

--- library/src/lib/loading-spinner/loading-spinner.component.ts
+++ library/src/lib/loading-spinner/loading-spinner.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'fd-loading-spinner',
     templateUrl: './loading-spinner.component.html',
     styleUrls: ['./loading-spinner.component.scss']
 })
-export class LoadingSpinnerComponent {
+export class LoadingSpinnerComponent implements OnChanges {
     @Input()
     loading: boolean = false;
+
+    constructor(private elRef: ElementRef){}
+
+    ngOnChanges() {
+        const parentEl = this.elRef.nativeElement.parentElement;
+        if (parentEl) {
+            parentEl.setAttribute('aria-busy', this.loading.toString());
+        }
+    }
 }
